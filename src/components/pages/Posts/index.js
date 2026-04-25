890d53c19a30d35ff7848efe218f977aed1f4cb6
2011Alexandrealvarenga <2011brunofernandes@gmail.com>
2026-04-25 11:51:41 -0300

    Instal tailwind and cards

--- src/components/pages/Posts/index.js
+++ src/components/pages/Posts/index.js
@@ -11,20 +11,22 @@ const Posts = () => {
     })
   }, []);
 
-
   return(
     <>
+    <div className='w-4/5 m-auto flex justify-between align-middle flex-wrap gap-10'>
       {
-        posts && posts.map((post)=>{
+        posts && posts.map(post =>{
           return(
-            <>
-              <h3>{post.title.rendered}</h3>
+            <div key={post.id} className="card p-3 w-[300px] shadow-lg rounded-lg flex flex-col border bg">
+              <img src="https://placehold.net/400x400.png" alt={post.title.rendered} />
+              <h3 className='text-lg font-bold'>{post.title.rendered}</h3>
               <p dangerouslySetInnerHTML={{__html: post.excerpt.rendered}}></p>
-            </>
+            </div>
           )
-      })
+        })
       
       }
+    </div>
     </>
   )
 }
